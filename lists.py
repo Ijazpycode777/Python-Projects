dcbfe88dfc3323b21f3a7b10625b1b6c6b5be3a1
Ijaz <ijazi3485@gmail.com>
2026-07-18 16:17:59 +0500

    Initial commit

---
+++ lists.py
@@ -0,0 +1,129 @@
+try:
+        with open('list1.txt','r') as file:
+                list1=file.read().splitlines()
+except:
+        list1=[ ]
+def save_task():
+        with open('list1.txt','w') as file:
+                for task in list1:
+                        file.write(task + "\n")
+print("1. Add tasks")
+print("2.Delete tasks")
+print("3.View tasks")
+print("4.Mark task as done")
+print("5.Unmark a task")
+print("6.Completed and remaining tasks.")
+print("7.Exit")
+while True:
+        choice=input("Enter your choice: ")
+        
+        if choice=="1":
+            while True:
+                task=input("Enter task or type 0 to return: ")
+                if task=="0":
+                    print("Returning to menu.")
+                    break
+                else:
+                    list1.append("[ ] "+task)
+                    print("Added succesfully!")
+                    save_task()
+                    break
+            
+        elif choice=="2":
+            if  not list1:
+                print("Nothing to delete!")
+            else:
+                for i in range(len(list1)):
+                        print(i+1,"-",list1[i])
+                while True:
+                    try:
+                        n=int(input("Enter task number to delete or enter 0 to return: "))
+                    except ValueError:
+                            print("Enter a valid number.")
+                            continue
+                    if n==0:
+                        print("No tasks deleted.")
+                        break
+                    elif 1<=n<= len(list1):
+                        list1.pop(n-1)
+                        print("Task deleted!")
+                        save_task()
+                        break
+                    else:
+                        print("Invalid input")
+                    
+        elif choice=="3":
+            if  not list1:
+                print("No tasks added yet.")
+            else:
+                for i in range(len(list1)):
+                    print(i+1,"-",list1[i])
+                    
+        elif choice=="4":
+            if not list1:
+                print("No task added yet.")
+            else:
+                for i in range(len(list1)):
+                    print(i+1,"-",list1[i])
+                while True:
+                    try:
+                        num=int(input("Enter task to be marked or type 0 to return: "))
+                    except ValueError:
+                        print("Enter a valid number.")
+                        continue
+                    if num==0:
+                        print("No tasks added")
+                        break
+                    elif 1<=num<=len(list1):
+                        if list1[num-1].startswith("[x]"):
+                            print("Task is already marked!")
+                        else:
+                           list1[num-1]=list1[num-1].replace("[ ] ","[x] ")
+                           save_task()
+                           print("Task marked.")
+                           break
+                    else:
+                        print("Invalid number!")
+        elif choice =="5":
+                 if not list1:
+                     print("No task added yet.\nReturning to menu.")
+                 else:
+                     for i in range(len(list1)):
+                        print(i+1,"-",list1[i])
+                     while True:
+                        try:
+                            num=int(input("Enter task to be marked or type 0 to return: "))
+                        except ValueError:
+                            print("Enter a valid number.")
+                            continue
+                        if num==0:
+                            print("No tasks added.\nReturning to menu.")
+                            break
+                        elif 1<=num<=len(list1):
+                            if list1[num-1].startswith("[ ]"):
+                                print("Task is already unmarked!")
+                                break
+                            else:
+                                list1[num-1]=list1[num-1].replace("[x] ","[ ] ")
+                                save_task()
+                                print("Task unmarked.")
+                                break
+                                        
+        elif choice == "6":
+                if not list1:
+                    print("No tasks added yet!")
+                else:
+                    completed=0
+                    for task in list1:
+                        if task.startswith("[x]"):
+                            completed+=1
+                    remaining = len(list1) - completed
+                    print("completed= ",completed)
+                    print("Remaining= ",remaining)
+                
+        elif choice=="7" or choice.strip().lower()=="no":
+                print("Goodbye!")
+                break
+        else:
+            print("Invalid input only between 1 and 7 or no to exit.")
+    
